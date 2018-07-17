bf254ac5aec64c6bb230dd49be87cfad58554929
ajshiff <ajshiffler@gmail.com>
2018-07-17 01:51:25 +0000

    Added needed blank files for moonlander. Defined lander methods and velocity methods. Updated makefile header.

---
+++ cs165/week06/assign06/lander.h
@@ -0,0 +1,26 @@
+#include "point.h"
+#include "velocity.h"
+
+using namespace std;
+
+class Lander {
+private:
+
+public:
+   Lander();
+   Point getPoint();
+   Velocity getVelocity();
+   bool isAlive();
+   bool isLanded() : 
+   int getFuel() : 
+   bool canThrust() : 
+   void setLanded(bool) : 
+   void setAlive(bool) : 
+   void setFuel(int) : 
+   void applyGravity(float);
+   void applyThrustLeft();
+   void applyThrustRight();
+   void applyThrustBottom();
+   void advance();
+   void draw();
+}
---
+++ cs165/week06/assign06/velocity.h
@@ -0,0 +1,14 @@
+using namespace std;
+
+class Velocity()
+{
+private:
+
+public:
+   Velocity()
+   Velocity(float, float)
+   float getDx();
+   float getDy();
+   void setDx(float);
+   void setDy(float);
+}
