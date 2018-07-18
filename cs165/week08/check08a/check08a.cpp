f14580509b61d90c3d4237ca2bbe644d6a85bdbf
ajshiff <ajshiffler@gmail.com>
2018-07-18 03:20:13 +0000

    Completed check08a.cpp

--- cs165/week08/check08a/check08a.cpp
+++ cs165/week08/check08a/check08a.cpp
@@ -1,9 +1,9 @@
 /***********************************************************************
 * Program:
 *    Checkpoint 08a, Inheritance
-*    Brother {Burton, Falin, Ercanbrack}, CS165
+*    Brother Cook, CS165
 * Author:
-*    your name
+*    Aaron Shiffler
 * Summary: 
 *    Summaries are not necessary for checkpoint assignments.
 * ***********************************************************************/
@@ -16,14 +16,92 @@ using namespace std;
 // in this file.
 
 // TODO: Define your Book class here
-
+class Book
+{
+private:
+   string title;
+   string author;
+   string publicationYear;
+public:
+   Book()
+   {
+      promptBookInfo();
+   }
+   void promptBookInfo()
+   {
+      cout << "Title: ";
+      getline(cin, title);
+      cout << "Author: ";
+      getline(cin, author);
+      cout << "Publication Year: ";
+      getline(cin, publicationYear);
+   };
+   void displayBookInfo()
+   {
+      cout << title << " (" << publicationYear << ") by " << author << endl;
+   };
+   void displayAll()
+   {
+      displayBookInfo();
+   };
+};
 
 // TODO: Define your TextBook class here
-
+class TextBook : Book
+{
+   private:
+      string subject;
+   public:
+      TextBook()
+      {
+         promptSubject();
+      }
+      void promptSubject()
+      {
+         cout << "Subject: ";
+         getline(cin, subject);
+      }
+      void displaySubject()
+      {
+         cout << "Subject: " << subject << endl;
+      }
+      void displayAll() 
+      {
+         displayBookInfo();
+         displaySubject();
+      }
+};
 
 // TODO: Add your PictureBook class here
+class PictureBook : Book
+{
+   private:
+      string illustrator;
+   public:
+      PictureBook()
+      {
+         promptIllustrator();
+      }
+      void promptIllustrator()
+      {
+         cout << "Illustrator: ";
+         getline(cin, illustrator);
+      }
+      void displayIllustrator()
+      {
+         cout << "Illustrated by " << illustrator << endl;
+      }
+      void displayAll()
+      {
+         displayBookInfo();
+         displayIllustrator();
+      }
+};
 
-
+void skipLine()
+{
+   cout << endl;
+}
 
 /**********************************************************************
  * Function: main
@@ -32,14 +110,19 @@ using namespace std;
 int main()
 {
    // Declare a Book object here and call its methods
-
-
+   Book normalBook;
+   skipLine();
+   normalBook.displayAll();
+   skipLine();
    // Declare a TextBook object here and call its methods
-
-
+   TextBook textBook;
+   skipLine();
+   textBook.displayAll();
+   skipLine();
    // Declare a PictureBook object here and call its methods
-
-
+   PictureBook pictureBook;
+   skipLine();
+   pictureBook.displayAll();
 
    return 0;
 }
