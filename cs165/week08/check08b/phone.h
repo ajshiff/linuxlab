7b14bbb7b514e08be9ace6780bfac248b63066f2
ajshiff <ajshiffler@gmail.com>
2018-07-18 18:16:18 +0000

    Created .h files for phone and smartPhone.

--- cs165/week08/check08b/phone.h
+++ cs165/week08/check08b/phone.h
@@ -1,10 +1,21 @@
 /***************
  * phone.h
  ***************/
-
+#pragma once
 #ifndef PHONE_H
 #define PHONE_H
+#include <string>
 
 // TODO: Put your phone class definition here...
+class Phone
+{
+private:
+    int areaCode;
+    int prefix;
+    int suffix;
+public:
+    void promptNumber();
+    void display();
+}
 
 #endif
--- cs165/week08/check08b/smartphone.h
+++ cs165/week08/check08b/smartphone.h
@@ -1,10 +1,18 @@
 /****************
  * smartphone.h
  ****************/
-
+#pragma once
 #ifndef SMARTPHONE_H
 #define SMARTPHONE_H
+#include <string>
 
 // TODO: Put your SmartPhone definition here
-
+class SnartPhone : Phone
+{
+private:
+    string email;
+public:
+    void prompt();
+    void display();
+}
 #endif
